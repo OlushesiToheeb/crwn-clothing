1cc5e04c2c1f6bee4e8d4942ef2baca70b43f11d
olushesitoheeb <olaolushesi98@gmail.com>
2020-08-29 02:50:59 -0700

    setUp the shop page

---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import SHOP_DATA from './shopData';
+import CollectionPreview from '../../components/collection-preview/collection-preview';
+
+class ShopPage extends React.Component {
+  state = {
+    collections: SHOP_DATA,
+  };
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className='shop-page'>
+        {collections.map(({ id, ...otherCollectionProp }) => (
+          <CollectionPreview key={id} {...otherCollectionProp} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
