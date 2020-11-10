165452cab47590228dd2629587c2496119d9c20a
olushesitoheeb <olaolushesi98@gmail.com>
2020-11-10 08:52:04 +0100

    convert selectCollection selector object into an array

--- src/components/collectionOverview/collectionOverview.jsx
+++ src/components/collectionOverview/collectionOverview.jsx
@@ -3,20 +3,20 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CollectionPreview from '../collection-preview/collection-preview';
-import { selectCollections } from '../../redux/shop/selector';
+import { selectCollectionsForPreview } from '../../redux/shop/selector';
 
 import './collectionOverview.scss';
 
 const CollectionsOverview = ({ collections }) => (
-  <div className='collections-overview'>
-    {collections.map(({ id, ...otherCollectionProp }) => (
-      <CollectionPreview key={id} {...otherCollectionProp} />
-    ))}
-  </div>
+    <div className='collections-overview'>
+        {collections.map(({ id, ...otherCollectionProp }) => (
+            <CollectionPreview key={id} {...otherCollectionProp} />
+        ))}
+    </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
+    collections: selectCollectionsForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
