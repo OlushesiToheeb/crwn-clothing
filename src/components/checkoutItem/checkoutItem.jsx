5a348045d4c8e9db54340eed160a30796b57d79b
olushesitoheeb <olaolushesi98@gmail.com>
2020-11-11 01:37:21 +0100

    ft - styled components

--- src/components/checkoutItem/checkoutItem.jsx
+++ src/components/checkoutItem/checkoutItem.jsx
@@ -1,38 +1,44 @@
 import React from 'react';
 
-import './checkoutItem.scss';
-
 import { connect } from 'react-redux';
 
 import {
-  clearItemFromCart,
-  removeItem,
-  addItem,
+    clearItemFromCart,
+    removeItem,
+    addItem,
 } from '../../redux/cart/actions';
 
-const CheckoutItem = ({ cartItem, dispatch }) => (
-  <div className='checkout-item'>
-    <div className='image-container'>
-      <img src={cartItem.imageUrl} alt='item' />
-    </div>
-    <span className='name'>{cartItem.name}</span>
-    <span className='quantity'>
-      <div className='arrow' onClick={() => dispatch(removeItem(cartItem))}>
-        &#10094;
-      </div>
-      <span className='value'>{cartItem.quantity}</span>
-      <div className='arrow' onClick={() => dispatch(addItem(cartItem))}>
-        &#10095;
-      </div>
-    </span>
-    <span className='price'>${cartItem.quantity * cartItem.price}</span>
-    <div
-      className='remove-button'
-      onClick={() => dispatch(clearItemFromCart(cartItem))}
-    >
-      &#10005;
-    </div>
-  </div>
-);
+import {
+    CheckoutItemContainer,
+    ImageContainer,
+    TextContainer,
+    QuantityContainer,
+    RemoveButtonContainer,
+} from './checkout-item.styles';
+
+const CheckoutItem = ({ cartItem, dispatch }) => {
+    const { name, imageUrl, price, quantity } = cartItem;
+    return (
+        <CheckoutItemContainer>
+            <ImageContainer>
+                <img src={imageUrl} alt='item' />
+            </ImageContainer>
+            <TextContainer>{name}</TextContainer>
+            <QuantityContainer>
+                <div onClick={() => dispatch(removeItem(cartItem))}>
+                    &#10094;
+                </div>
+                <span>{quantity}</span>
+                <div onClick={() => dispatch(addItem(cartItem))}>&#10095;</div>
+            </QuantityContainer>
+            <TextContainer>${quantity * price}</TextContainer>
+            <RemoveButtonContainer
+                onClick={() => dispatch(clearItemFromCart(cartItem))}
+            >
+                &#10005;
+            </RemoveButtonContainer>
+        </CheckoutItemContainer>
+    );
+};
 
 export default connect()(CheckoutItem);
