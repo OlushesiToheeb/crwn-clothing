c7a36f60488445c88e9eefca739edce7e3d89858
tolushesi-tme <tolushesi@techmadeeazy.com>
2021-01-18 12:39:56 +0100

    updated app and signIn component to useHooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './App.css';
@@ -11,35 +11,29 @@ import { selectCurrentUser } from './redux/user/selector';
 import { checkUserSession } from './redux/user/actions';
 import { createStructuredSelector } from 'reselect';
 
-class App extends React.Component {
-    componentDidMount() {
-        const { dispatch } = this.props;
+const App = ({ dispatch, currentUser }) => {
+    useEffect(() => {
         dispatch(checkUserSession());
-    }
-    render() {
-        return (
-            <div>
-                <Header />
-                <Switch>
-                    <Route exact path='/' component={HomePage} />
-                    <Route path='/shop' component={ShopPage} />
-                    <Route exact path='/checkout' component={Checkout} />
-                    <Route
-                        exact
-                        path='/signin'
-                        render={() =>
-                            this.props.currentUser ? (
-                                <Redirect to='/' />
-                            ) : (
-                                <SignInSignUp />
-                            )
-                        }
-                    />
-                </Switch>
-            </div>
-        );
-    }
-}
+    }, [dispatch]);
+
+    return (
+        <div>
+            <Header />
+            <Switch>
+                <Route exact path='/' component={HomePage} />
+                <Route path='/shop' component={ShopPage} />
+                <Route exact path='/checkout' component={Checkout} />
+                <Route
+                    exact
+                    path='/signin'
+                    render={() =>
+                        currentUser ? <Redirect to='/' /> : <SignInSignUp />
+                    }
+                />
+            </Switch>
+        </div>
+    );
+};
 
 const mapStateToProps = createStructuredSelector({
     currentUser: selectCurrentUser,
--- src/components/signIn/signIn.jsx
+++ src/components/signIn/signIn.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 
 import './signIn.scss';
@@ -6,62 +6,60 @@ import FormInput from '../form-input/formInput';
 import CustomButton from '../custom-button/customButton';
 import { googleSignInStart, emailSignInStart } from '../../redux/user/actions';
 
-class SignIn extends React.Component {
-    state = {
+const SignIn = ({ dispatch }) => {
+    const [userCredentials, setCredentials] = useState({
         email: '',
         password: '',
-    };
+    });
 
-    handleSubmit = async (e) => {
+    const { email, password } = userCredentials;
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        const { email, password } = this.state;
-        const { dispatch } = this.props;
         dispatch(emailSignInStart({ email, password }));
     };
 
-    handleChange = (e) => {
+    const handleChange = (e) => {
         const { value, name } = e.target;
-        this.setState({ [name]: value });
+        setCredentials({ ...userCredentials, [name]: value });
     };
-    render() {
-        const { dispatch } = this.props;
-        return (
-            <div className='sign-in'>
-                <h2>I already have an account</h2>
-                <span>Sign in with your email and password</span>
 
-                <form onSubmit={this.handleSubmit}>
-                    <FormInput
-                        type='email'
-                        name='email'
-                        value={this.state.email}
-                        handleChange={this.handleChange}
-                        label='Email'
-                        required
-                    />
+    return (
+        <div className='sign-in'>
+            <h2>I already have an account</h2>
+            <span>Sign in with your email and password</span>
 
-                    <FormInput
-                        type='password'
-                        name='password'
-                        value={this.state.password}
-                        label='Password'
-                        handleChange={this.handleChange}
-                        required
-                    />
-                    <div className='buttons'>
-                        <CustomButton type='submit'>SIGN IN</CustomButton>
-                        <CustomButton
-                            type='button'
-                            onClick={() => dispatch(googleSignInStart())}
-                            isGoogleSignIn
-                        >
-                            SIGN IN WITH GOOGLE
-                        </CustomButton>
-                    </div>
-                </form>
-            </div>
-        );
-    }
-}
+            <form onSubmit={handleSubmit}>
+                <FormInput
+                    type='email'
+                    name='email'
+                    value={email}
+                    handleChange={handleChange}
+                    label='Email'
+                    required
+                />
+
+                <FormInput
+                    type='password'
+                    name='password'
+                    value={password}
+                    label='Password'
+                    handleChange={handleChange}
+                    required
+                />
+                <div className='buttons'>
+                    <CustomButton type='submit'>SIGN IN</CustomButton>
+                    <CustomButton
+                        type='button'
+                        onClick={() => dispatch(googleSignInStart())}
+                        isGoogleSignIn
+                    >
+                        SIGN IN WITH GOOGLE
+                    </CustomButton>
+                </div>
+            </form>
+        </div>
+    );
+};
 
 export default connect()(SignIn);
